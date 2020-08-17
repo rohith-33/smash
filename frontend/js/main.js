de733ff53b30031f3dc4226eb482761a3dbac540
rohith-33 <rohithmuthyala3@gmail.com>
2020-08-17 06:21:49 +0530

    form

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,31 +1,55 @@
-function showdata()
- {
-    $.ajax({
-        url: "/api/user",
-        method: "GET",
-        success: function(result) {
-            var r="<table>";
-            r+="<tr>"
-            r+="<td>"+"Username"+"</td>";
-            r+="<td>"+"Age"+"</td>";
-            r+="<td>"+"email"+"</td>";
-            r+="</tr>";
-            for(var row in result)
-            {   r+="<tr>";
-                r+="<td>"+result[row].username+"</td>";
-                r+="<td>"+result[row].age+"</td>";
-                r+="<td>"+result[row].email+"</td>";
-                r+="</tr>";
-            }
-            r+="</table>";
-            $("#table").html(r);
-        },
-        error: function(err) {
-          console.log(err);
+$(function(){
+  $("#submit").on("click",()=>{
+      var jsondata={
+          id:$("#_id").val(),
+          name:$("#name").val(),
+          lname:$("#lname").val(),
+          em:$("#em").val(),
+          age:$("#age").val(),
+          dob:$("#dob").val(),
+          mobile:$("#mobile").val()
         }
-      });
-     
-     $("button").click(function(){
-        $("#messages").css("display","inline");
- })}
- showdata();
+        $.ajax({
+            type:"post",
+            url:"/api/users/create",
+            data: jsondata,
+            success: function(){
+               $("h5").html("your data is stored succesfully").hide().fadeIn("slow");
+               setTimeout(() => {
+                  location.reload();
+               }, 2000);
+                   
+            } ,
+            error:(err)=>{
+              $("h5").html("sorry failed please try again").hide().fadeIn(slow);
+              setTimeout(() => {
+                 location.reload();
+              }, 2000);
+            }
+        })
+  })
+
+ $("#view").on('click',function(){$.ajax({
+  type:"get",
+  url:"/api/users",
+  success:function(data){
+          display(data);
+  },
+  error: (err)=>{
+      console.log("ERROR");
+  }
+})})
+  
+
+
+
+function display(data){
+  var txt="<table><tr><th>ID</th><th>FIRSTNAME</th><th>LASTNAME</th><th>Email</th><th>AGE</th><th>DOB</th><th>MOBILE</th></tr>";
+   for(var i=0;i<data.length;i++){
+       txt+="<tr><td>"+data[i].id+"</td><td>"+data[i].name+"</td><td>"+data[i].lname+"</td><td>"+data[i].em+"</td><td>"+data[i].age+"</td><td>"+data[i].dob+"</td><td>"+data[i].mobile+"</td"+"</tr>";
+   }
+   txt+="</table>";
+   $("#table").html(txt);
+}
+
+})
